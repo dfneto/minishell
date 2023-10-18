e166a3762a22293e7143849e35d451768857579f
davifern <pdavidfneto@gmail.com>
2023-10-18 18:52:28 +0200

    corrigido quando s1 fosse null

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,16 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lsulzbac <lsulzbac@student.42barcel>       +#+  +:+       +#+        */
+/*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 14:31:14 by lsulzbac          #+#    #+#             */
-/*   Updated: 2022/05/16 14:38:18 by lsulzbac         ###   ########.fr       */
+/*   Updated: 2023/10/18 18:32:18 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stddef.h>
 
+#include <stdio.h>
 char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*r;
@@ -24,7 +25,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	r = (char *) malloc (s1_len + ft_strlen(s2) + 1);
 	if (r != NULL)
 	{
-		while (s1[i])
+		while (s1 && s1[i])
 		{
 			r[i] = s1[i];
 			i++;
