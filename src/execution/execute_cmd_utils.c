04dbf40bd759faf47979056f62057bc77be9eb30
mint kinhas <lsr.jupi@gmail.com>
2023-12-22 10:51:47 +0100

    seg fault - double free ok

--- src/execution/execute_cmd_utils.c
+++ src/execution/execute_cmd_utils.c
@@ -62,7 +62,7 @@ char	*get_path(char **cmd, t_env env)
 	if (cmd[0][0] == '\0')
 		return (NULL);
 	if (is_cmd_executable(cmd[0]) && !access(cmd[0], X_OK))
-		return (cmd[0]);
+		return (ft_strdup(cmd[0]));
 	abs_path = (char *)ft_calloc(PATH_MAX, sizeof(char));
 	if (!abs_path)
 		return (NULL);
