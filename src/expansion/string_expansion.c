9b0bb88b20b09b2189c10248e768e0352f7c63bf
mint kinhas <lsr.jupi@gmail.com>
2024-01-20 11:52:24 +0100

    skip not expansible token

--- src/expansion/string_expansion.c
+++ src/expansion/string_expansion.c
@@ -40,12 +40,19 @@ t_token	*expand_token_dolar(t_token *token, char *pre_dolar, t_env env)
 
 	next_tok_after_expand = token->next;
 
+	//ADICIONEI ESSE TESTE PARA EVITAR DE CRIAR O TOKEN QUANDO A VARIAVEL NAO EXISTE NO ENV
+	if (!exist_in_env(token->str, env))
+	{
+		token->str = NULL;
+		token->type = SPC;
+		return (token);
+	}
 	if (token->str == NULL)
 		expanded_str = safe_strdup("");
 	else
 		expanded_str = safe_strdup(ft_getenv(token->str, env));
-	if (expanded_str == NULL)
-		return (set_token_str(token, ""));
+	//if (expanded_str == NULL)     -> RETIREI ESSA PARTE PORQUE O SAFE STRDUP NUNCA VAI RETORNAR NULL!
+	//	return (set_token_str(token, ""));
 	token->str = NULL;
 	if (has_space(expanded_str))
 		return (create_tok_per_word_in(expanded_str, pre_dolar, next_tok_after_expand, token));
