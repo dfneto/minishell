4456b4856079035d4fe6a0f6e248b7d2c510ab53
david42 <davifern@cbr5s5.42barcelona.com>
2024-01-21 20:24:23 +0100

    fixing errors of double quotes expansion

--- src/expansion/double_quotes_expansion.c
+++ src/expansion/double_quotes_expansion.c
@@ -6,7 +6,7 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 17:54:32 by davifern          #+#    #+#             */
-/*   Updated: 2024/01/21 15:37:17 by davifern         ###   ########.fr       */
+/*   Updated: 2024/01/21 20:23:36 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,12 @@ char	*get_word_expanded(t_token *token, int *i, int dolar_position,
 		t_env env)
 {
 	char	*word_to_expand;
+
 	word_to_expand = NULL;
 	if (token->str[*i] == '?')
 	{
 		(*i)++;
-		return get_exit_status();
+		return (get_exit_status());
 	}
 	while (token->str[*i] && is_alnum_or_slash(token->str[*i]))
 		(*i)++;
@@ -28,34 +29,59 @@ char	*get_word_expanded(t_token *token, int *i, int dolar_position,
 			- dolar_position - 1);
 	if (word_to_expand == NULL)
 		return (safe_strdup(""));
+	// printf("word to expand: %s\n", word_to_expand);
 	return (safe_strdup(ft_getenv(word_to_expand, env)));
 }
 
-/*
-caso 1: $USER 				david 		- ok
-caso 2: $$USER				$david 		- ok
-caso 3: $hola						 	- ok
-caso 4: $$hola				$			- ok
-caso 5: $					$			- ok
-caso 6: $$					$$			- ok
-caso 8: echo "hi$"    		hi$     	- ok
-caso 9: echo "hi$$"   		hi$$    	- ok
-caso 13: echo "'$USER'"		'david'		- ok
+char	*expand_post_dollar(t_token *token, int *i, int dolar_position,
+		t_env env)
+{
+	char	*word_expanded;
+	char	*post_word;
 
-caso 7: echo "$/"     $/      - ??? decidi não tratar porque não encontrei a lógica. Ex: hi$/ e hi$& imprimem igual, mas hi$!, hi$(, hi$* imprime outra coisa
-caso 10: echo "hi$/"  hi$/    - decidi não tratar porque não encontrei a lógica. Ex: hi$/ e hi$& imprimem igual, mas hi$!, hi$(, hi$* imprime outra coisa
-caso 11: echo "hi$$/"  hi$$/  - decidi não tratar porque não encontrei a lógica. Ex: hi$/ e hi$& imprimem igual, mas hi$!, hi$(, hi$* imprime outra coisa
-caso 12: echo "'$'"
+	word_expanded = NULL;
+	post_word = NULL;
+	word_expanded = get_word_expanded(token, i, dolar_position, env);
+	// printf("word to expand: %s\n", word_expanded);
+		
+	// joined = safe_strjoin(joined, word_expanded);
+	// get_pos_word
+	// if (token->str[*i] != '$') // para os casos 38, 39 e 40 echo "|$USER|"
+	// {
+	// 	post_word = safe_substr(token->str, *i, ft_strlen(token->str) - *i);
+	// 	word_expanded = safe_strjoin(word_expanded, post_word);
+	// }
+	return (word_expanded);
+}
+
+/*
+* cases: "$USER"
+* 		 "123$USER" - with pre dolar
+*		 "123$USER $USER"
+* Others cases:
+caso 1: echo "$USER" 				david 		- ok
+caso 2: echo "$$USER"				$david 		- ok
+caso 3: echo "$hola"								- ok
+caso 4: echo "$$hola"				$			- ok
+caso 5: echo "$""					$			- ok
+caso 6: echo "$$""					$$			- ok
+caso 8: echo "hi$"    				hi$     	- ok
+caso 9: echo "hi$$"   				hi$$    	- ok
+caso 13: echo "'$USER'"				'david'		- ok
+caso 7: echo "$/"		     		$/      	- ok
+caso 10: echo "hi$/"  				hi$/    	- ok
+caso 11: echo "hi$$/"  				hi$$/ 		- ok
+caso 12: echo "'$'"					'$'			- ok
+caso 13: echo "$USER$PATH$PWD"		imprime...	- ok
 */
-//echo "$USER$PATH$PWD"
 t_token	*expand_double_quote_token(t_token *token, t_env env)
 {
-	int i;
-	int dolar_position;
-	char *pre_dolar;
-	char *joined;
-	char *word_expanded;
-	char *post_word;
+	int		i;
+	int		dolar_position;
+	char	*pre_dolar;
+	char	*joined;
+	char	*word_expanded;
+	char	*post_word;
 
 	i = 0;
 	dolar_position = 0;
@@ -63,27 +89,41 @@ t_token	*expand_double_quote_token(t_token *token, t_env env)
 	joined = NULL;
 	word_expanded = NULL;
 	post_word = NULL;
+	
+	// print_list_token(token);
+
 	while (token->str[i] && dolar_position >= 0)
 	{
-		pre_dolar = get_pre_dolar_text(token->str, &dolar_position, i);
-		joined = safe_strjoin(joined, pre_dolar);
+		// pre_dolar = get_pre_dolar_text(token->str, &dolar_position, i);
+		joined = safe_strjoin(joined, get_pre_dolar_text(token->str, &dolar_position, i));
 		if (dolar_position >= 0)
 			i = dolar_position + 1;
 		if (token->str[i] && dolar_position >= 0)
 		{
-			word_expanded = get_word_expanded(token, &i,
-						dolar_position, env);
-			joined = safe_strjoin(joined, word_expanded);
-			//get_pos_word
-			if (token->str[i] != '$') //para os casos 38, 39 e 40 echo "|$USER|"
-			{
-				post_word = safe_substr(token->str, i, ft_strlen(token->str) - i);
-				joined = safe_strjoin(joined, post_word);
-			}
+			joined = safe_strjoin(joined, expand_post_dollar(token, &i, dolar_position, env));
+			// printf("i: %d joined: %s\n", i, joined);
 		}
-		else if (token->str[i-1] == '$') //para os casos 5 e 6
+		// {
+		// 	word_expanded = get_word_expanded(token, &i, dolar_position, env);
+			
+		// 	joined = safe_strjoin(joined, word_expanded);
+		// 	// get_pos_word
+		// 	if (token->str[i] != '$') // para os casos 38, 39 e 40 echo "|$USER|"
+		// 	{
+		// 		post_word = safe_substr(token->str, i, ft_strlen(token->str) - i);
+		// 		joined = safe_strjoin(joined, post_word);
+		// 	}
+		// }
+		else if (token->str[i - 1] == '$') // para os casos 5 e 6
 			joined = safe_strjoin(joined, "$");
 	}
+	// get_pos_last_word
+	if (token->str[i] != '$') // para os casos 38, 39 e 40 echo "|$USER|"
+	{
+		post_word = safe_substr(token->str, i, ft_strlen(token->str) - i);
+		joined = safe_strjoin(joined, post_word);
+	}
 	token->str = joined;
+	// print_token(token);
 	return (token);
 }
